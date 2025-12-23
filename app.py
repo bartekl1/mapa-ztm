0a08989497a745e5866c26e1562dcb1e9c0eb8c8
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2025-12-23 18:38:54 +0100

    Download cache during Flask app creation, renamed save_cache.py to download_cache.py

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import os
 
 from modules.config import load_config
 from modules.gtfs_functions import get_current_positions, get_shape, get_route_info_by_trip, get_stops_on_trip, get_stops, get_trip_details
-from save_cache import save_cache
+from download_cache import download_cache
 
 def create_app(config: dict | None = None) -> Flask:
     if config is None:
         config = load_config()
     cache_path = config.get("gtfs_cache_path", "gtfs_cache.db")
+
+    if not os.path.exists(config.get("gtfs_cache_path", "gtfs_cache.db")):
+        download_cache(config)
+
     app = Flask(__name__, static_folder="frontend/dist", static_url_path="/")
 
     @app.route("/")
@@ -54,8 +58,6 @@ def create_app(config: dict | None = None) -> Flask:
 
 def run_app():
     config = load_config()
-    if not os.path.exists(config.get("gtfs_cache_path", "gtfs_cache.db")):
-        save_cache(config)
     app = create_app(config)
     app.run(**config.get("flask", {}))
 
