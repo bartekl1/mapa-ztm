0bd20a93817843b22b0b19af220aac9e5e3ed0cf
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2026-09-15 18:38:07 +0200

    Empty map

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./gui/App.jsx";
+import { initMap } from "./map/map.js";
+
+initMap();
 
 createRoot(document.getElementById("react-root")).render(
     <StrictMode>
---
+++ frontend/src/map/map.js
@@ -0,0 +1,43 @@
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+import "leaflet.markercluster";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+
+import { setWorkerUrl } from 'maplibre-gl';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
+import { maplibreGL } from "@maplibre/maplibre-gl-leaflet";
+import "maplibre-gl/dist/maplibre-gl.css";
+setWorkerUrl(workerUrl);
+
+import "./map.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl,
+    iconRetinaUrl,
+    shadowUrl,
+});
+
+export function initMap() {
+    const map = L.map("map");
+
+    map.setView([52.40, 16.96], 13);
+
+    // const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //     minZoom: 4,
+    //     maxZoom: 19,
+    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    //     className: "map-tiles",
+    // }).addTo(map);
+    const gl = maplibreGL({
+        style: "https://tiles.openfreemap.org/styles/liberty",
+        // style: "https://tiles.openfreemap.org/styles/bright",
+    }).addTo(map);
+    map.attributionControl.addAttribution('<a href="https://www.ztm.poznan.pl/otwarte-dane/dla-deweloperow/">API ZTM Poznań</a>');
+}
